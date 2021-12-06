c618e9cc59aa4fe354dfe8b088e4bad5ae625f84
LoneRonin <43432734+LoneRonin@users.noreply.github.com>
2021-12-06 04:17:24 -0800

    Assignment 1-1
    main created and moved files into individual folder for assignments

---
+++ Assignment 1-1/assignment 1-1.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+void findStudent(ifstream &, int);
+
+struct Student {
+    int id;
+    char StuName[20];
+    double score[2];
+    double sum, avg;
+};
+
+int main(){
+    ifstream ifs;
+    ofstream ofs;
+    Student s;
+    int IdNum;
+
+    ifs.open("student.txt");
+    ofs.open("student.bin");
+
+    for(int i=0; i<=9; i++) {
+        ifs >> s.id;
+        ifs >> s.StuName;
+        ifs >> s.score[0]; 
+        ifs >> s.score[1];
+        s.sum = s.score[0] + s.score[1];
+        s.avg = s.sum/2.0;
+
+        ofs.write((char*)&s, sizeof(s));
+
+        cout << "ID from Bin: " << s.id
+            << "/n Name from Bin: " << s.StuName
+            << "/n Average from Bin: " << s.avg << endl;
+    }
+
+    ifs.close();
+    ofs.close();
+
+    ifs.open("student.bin");
+    cout << "Search Student ID number: " << endl;
+    cin >> IdNum;
+    findStudent(ifs, IdNum);
+};
+
+void findStudent(ifstream &ifs, int IdNum) {
+    
+}
