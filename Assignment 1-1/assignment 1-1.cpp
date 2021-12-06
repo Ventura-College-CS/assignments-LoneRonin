8977924b3d5c72efde255b24da1d2e84444d3361
LoneRonin <43432734+LoneRonin@users.noreply.github.com>
2021-12-06 05:15:03 -0800

    update assignment 1
    Cleaned search function and gave a not found id case

--- Assignment 1-1/assignment 1-1.cpp
+++ Assignment 1-1/assignment 1-1.cpp
@@ -31,9 +31,9 @@ int main(){
 
         ofs.write((char*)&s, sizeof(s));
 
-        cout << "ID from Bin: " << s.id
-            << "/n Name from Bin: " << s.StuName
-            << "/n Average from Bin: " << s.avg << endl;
+        cout << "ID from Bin: " << s.id << endl
+            << "Name from Bin: " << s.StuName << endl
+            << "Average from Bin: " << s.avg << endl;
     }
 
     ifs.close();
@@ -45,19 +45,26 @@ int main(){
     findStudent(ifs, IdNum);
 };
 
+
 void findStudent(ifstream &ifs, int IdNum) {
     Student s;
-    
+    int StudentIdFound = 0;
+
     //need to make case for not found id
-    if(IdNum == s.id) {
-        
-        s.sum = s.score[0] + s.score[1];
-        s.avg = s.sum / 2.0;
-		cout << "ID: " <<s.id
-		    << "\nName: " << s.StuName
-		    << "\nScore 1: " << s.score[0]
-		    << "\nscore 2: " <<s.score[1]
-		    << "\nSum  : " << s.sum
-		    << "\nAvg  : " << s.avg << endl;
+    for (int i=0; i<=9; i++) {
+        ifs.read((char*)&s, sizeof(s));
+        if(IdNum == s.id) {
+            s.sum = s.score[0] + s.score[1];
+            s.avg = s.sum / 2.0;
+	    	cout << "ID: " <<s.id << endl
+		        << "Name: " << s.StuName << endl
+		        << "Score 1: " << s.score[0] << endl
+		        << "Score 2: " <<s.score[1] << endl
+		        << "Sum  : " << s.sum << endl
+		        << "Avg  : " << s.avg << endl;
+            StudentIdFound++;
+        }
     }
+    if (StudentIdFound == 0)
+        cout << "Student ID Not Found" << endl;
 }
