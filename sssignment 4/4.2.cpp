4b18f446dd09773c4413d1016f822be5fde30105
LoneRonin <43432734+LoneRonin@users.noreply.github.com>
2021-12-06 18:14:50 -0800

    Update 4.2.cpp

--- sssignment 4/4.2.cpp
+++ sssignment 4/4.2.cpp
@@ -4,8 +4,7 @@
         target = array[i] 0<i<64
         all values must be called after sortArray
     purpose of for loop is to average the comparison of numbers from linearSearch and binarySearch
-results should be close to 64/2 and (1+6)/2
-*/
+results should be close to 64/2 and (1+6)/2 */
 
 #include <iostream>
 #include <ctime>
@@ -16,7 +15,57 @@ void makeArray(int [], int);    //fill 64 elements with random numss between 1-1
 int linearSearch(int [], int, int); //return value # of comparisons used to findd target
 int binarySearch(int [], int, int); //return value # of comparisons used to findd target
 void sortArray(int [], int);    // call before binarySearch
+void printArray(int [], int);   //print array contents
 
 int main(){
-    
+    const int N = 64;
+    int array[N];
+    int target;
+    int total_linear, total_binary;
+
+    srand(time(0));
+    makeArray(array, N);
+    printArray(array, N);
+
+    for (int i=0; i<100; i++) {
+        target = array[rand() % N];
+        total_linear += linearSearch(array, N, target) ;
+    }
+    cout << "The average comparision numbers of Linear Search is " << total_linear / 100.0 << endl;
+
+    sortArray(array, N);
+    printArray(array, N);
+
+    for(int i=0; i<100; i++) {
+        target = array[rand() % N];
+        total_binary += binarySearch(array, N, target) ;
+    }
+    cout << "The average comparision numbers of Binary Search is " << total_binary / 100.0 << endl;
 }
+
+void makeArray(int array[], int N) {
+    for(int i=0; i<N; i++) 
+        array[i] = rand() % 100 + 1;
+}; 
+int linearSearch(int array[], int N, int target) {
+    
+}; 
+int binarySearch(int array[], int N, int target) {
+
+}; 
+void sortArray(int array[], int N) {
+    int min, index;
+    for(int i=0; i<N-1; i++) {
+        for(int j=0; j<N-i-1; j++) {
+            if (array[j] > array[j+1])
+                swap(array[j], array[j+1]);
+        }
+    }
+};
+void printArray(int array[], int N) {
+    for(int i=0; i<N; i++) {
+        cout << array[i] << "\t";
+        if (i % 8 == 7)
+            cout << endl;
+    }
+};
