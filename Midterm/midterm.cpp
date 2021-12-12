f7c59ccacc51e5e50059034d66844049e95008bd
LoneRonin <43432734+LoneRonin@users.noreply.github.com>
2021-12-12 06:53:25 -0800

    Update midterm.cpp
    Started to initialized individuals names and scores. And put them into arrays that are of class SStudent

--- Midterm/midterm.cpp
+++ Midterm/midterm.cpp
@@ -4,9 +4,16 @@
 using namespace std;
 
 int main () {
-    
-}
+    Student s[5]; //Array of Class Students being initialized
+    string name[] = {"Takeo", "Thy", "Yuumi", "Haru", "Irelia"}; //Array filled with nammes
+    double scores[5][5] = {
+        {87.0, 98.0, 100.0}, {100.0, 78.0, 65.0}, {100.0, 100.0, 100.0}, {83.0, 90.0, 60.0},{92.0, 86.0, 100.0}
+    };
 
+    Student tmp;
+    vector<double> C;
+    Stack<Students
+}
 //Needs to do for main
 // initialize student array
 // double array for scores
