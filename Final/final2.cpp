0f1c2a8a1f5e8845b4039e63be3eae46f6efd0d8
LoneRonin <43432734+LoneRonin@users.noreply.github.com>
2021-12-13 01:33:18 -0800

    Update final2.cpp
    moved over main from prob 1

--- Final/final2.cpp
+++ Final/final2.cpp
@@ -8,7 +8,9 @@ class Course {
         int cCred;
     public:
         Course() : cId(0), cName(), cCred() {
-
+            cId = id;
+            cName = name;
+            cCred = cred;
         }
         int getCId() const {
             return cId;
@@ -29,3 +31,36 @@ class Course {
             cCred = cred;
         }
 };
+
+int main() {
+    Course C[10];
+    string classes[] = {"Chemistry", "Algebra", "English", "Geometry", "Geography", "Biology",
+    "Calculus", "Physics", "Literature", "Communication"};
+    int Credits[10] = {6, 4, 5, 3, 2, 5, 4, 4, 3, 3};
+
+    for (int i = 0; i < 10; i++) {
+        C[i].setCId(i);
+        C[i].setCName(classes[i]);
+        C[i].setCCred(Credits[i]);
+    }
+
+    int target = 0;
+
+    cout << "Enter 0-9 to Search a Course Id: ";
+    cin >> target;
+
+    int n = sizeof(c)/sizeof(c[0]);
+    int first = 0, last = n - 1;
+    int result = BinSearch(c, first, last, target);
+
+    if (result <= 9 || result == 0) {
+        cout << "Course Name: " << c[target].getCName() << endl;
+        cout << "Course Id: V0" << c[target].getCId() << endl;
+        cout << "Course Credits: " << c[target].getCCred() << endl;
+    }
+    else {
+        cout << "No Course with that Id Found";
+    }
+
+    return 0;
+}
