287ff66eb7bd77604f982f8e0e76be2c06a17243
LoneRonin <43432734+LoneRonin@users.noreply.github.com>
2021-12-13 00:14:20 -0800

    Update final1.cpp
    Finished class course

--- Final/final1.cpp
+++ Final/final1.cpp
@@ -1,3 +1,39 @@
 #include <iostream>
 using namespace std;
 
+Class Course {
+    private:
+        int cId;
+        string cName;
+        int cCred;
+    public:
+        Course() : cID(0), cName(), cCred() {
+
+        }
+        int getcId() const {
+            return cId;
+        }
+        string getCName() const {
+            return cName;
+        }
+        int getCCred() const {
+            return cCred;
+        }
+        void setCId(int id) {
+            cId = id;
+        }
+        void setCName(string name) {
+            cName = name;
+        }
+        void setCCred(int cred) {
+            cCred = cred;
+        }
+};
+
+int BinSearch() {
+    
+}
+
+int main() {
+
+}
