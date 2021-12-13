57dbc5e27e46c8a3877680f5467e2acca8cfbec4
LoneRonin <43432734+LoneRonin@users.noreply.github.com>
2021-12-13 00:43:16 -0800

    Update final1.cpp
    Fixed syntax for class and changed Id values.
    Started other stuff

--- Final/final1.cpp
+++ Final/final1.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 
-Class Course {
+class Course {
     private:
         int cId;
         string cName;
@@ -11,7 +11,7 @@ Class Course {
         Course() : cID(0), cName(), cCred() {
 
         }
-        int getcId() const {
+        int getCId() const {
             return cId;
         }
         string getCName() const {
@@ -42,12 +42,22 @@ int main() {
     int Credits[10] = {6, 4, 5, 3, 2, 5, 4, 4, 3, 3};
 
     for (int i = 0; i < 10; i++) {
-        C[i].setCId(i + 100);
+        C[i].setCId(i);
         C[i].setCName(classes[i]);
         C[i].setCCred(Credits[i]);
     }
-    //temp test loop
+
+    // Now to create the Binary search part and the print results part
+
+    int target = 0;
+
+    cout << "Enter 0-9 to Search a Course Id: ";
+    cin >> target;
+
+    
+
+    /*temp test loop
     for (int i = 0; i < 10; i++) {
-        cout << "ID: " << C[i].getCId << " Class: " << C[i].getCName << " Credits: " << C[i].getCCred;
-    }
+        cout << "ID: " << C[i].getCId() << " Class: " << C[i].getCName() << " Credits: " << C[i].getCCred() << endl;
+    }*/
 }
