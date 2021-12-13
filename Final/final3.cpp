5f06e8a7059752ee5983dc4670af0a93356221b0
LoneRonin <43432734+LoneRonin@users.noreply.github.com>
2021-12-13 14:19:30 -0800

    Update final3.cpp
    Made my Postfix and Stack classes

--- Final/final3.cpp
+++ Final/final3.cpp
@@ -1,4 +1,48 @@
 #include <iostream>
+#include <vector>
+#include <queue>
 using namespace std;
 
-Class
+class PostfixExp
+{
+private:
+    string expression;
+    int isOperator(char);
+    int calculate(int, int, char);
+
+public:
+    PostfixExp();
+    PostfixExp(string);
+    void setExpression(string);
+    void printExpression() const;
+    int Evaluation();
+};
+
+template<class T, int capacity=30>
+class Stack
+{
+
+private:
+    vector<T>  pool;
+public:
+    Stack() {
+        pool.reserve(capacity);
+    }
+    Stack(int size) {
+        pool.reserve(size);
+    }
+    T pop() {
+        T val = pool.back();
+        pool.pop_back();
+        return val;
+    }
+    void push(T v) {
+        pool.push_back(v);
+    }
+    T isEmpty() const {
+        return pool.empty();
+    }
+    T &topEl() {
+        return pool.back();
+    }
+};
