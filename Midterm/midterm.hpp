24768e9ad349af65215660c9a7a16e9884f4a8f3
LoneRonin <43432734+LoneRonin@users.noreply.github.com>
2021-12-12 18:37:02 -0800

    Update midterm.hpp
    Moved Student Class

--- Midterm/midterm.hpp
+++ Midterm/midterm.hpp
@@ -6,44 +6,6 @@
 
 using namespace std;
 
-class Student {
-    private:
-        int SID;
-        string Sname;
-        vector<double> scores;
-    public:
-        Student() : SID(0), Sname() {}
-        Student(int i, string name, double *sc) {
-            SID = i;
-            Sname = name;
-            for(int j = 0; j<5; j++) { // possible idea... prehave all 5 objects made in line 16 and then have it just run each object idv.
-                scores.push_back(j); //come back to this to figure out how to have all 5
-                                     //Sstudent Objectyss be pushed
-            }
-        }
-        int getID() {
-            return SID;
-        }
-        string getName() {
-            return Sname;
-        }
-        vector<double> &getScores() {
-            return scores;
-        };
-        void setID(int ID) {
-            SID = ID;
-        }
-        void setName(string name) {
-            Sname = name;
-        }
-        void setScores(double *sc) {
-            //hmmmm maybe sstart on int main now
-            scores.clear();
-            for (int i = 0; i < 5; i++) {
-                scores.pussh_back(sc[i]);
-            }
-};
-
 template <class T, int capacity = 30>
 class Stack {
     private:
