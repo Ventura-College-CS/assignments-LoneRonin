a3ccb226ac0cc59713c14f6683ebec1277cd6002
LoneRonin <43432734+LoneRonin@users.noreply.github.com>
2021-12-13 00:52:43 -0800

    Update final1.cpp
    Moved a previous binseaarch function to
    Finishing up main function with its main outputs aas well as you you interact with program

--- Final/final1.cpp
+++ Final/final1.cpp
@@ -31,9 +31,29 @@ class Course {
         }
 };
 
-int BinSearch() {
+int BinSearch(int array[], int first, int last, int target) { // need to edit to impliment with objects
+    int mid; // middle point of the search
 
-}
+    if (first > last)
+        return -1;
+    mid = (first + last)/2;
+    if (array[mid] == target)
+        return mid;
+    if (array[mid] < target)
+        return binarySearch(array, mid + 1, last, target);
+    else
+        return binarySearch(array, first, mid - 1, target);
+}  
+
+/*
+    The BinSearch function performs a recursive binary search on range of 
+    elements of an integer arrary that is passed through a parameter array.
+    This method checks through the whole array to see whether or not the search
+    value is held within and returns -1 if not within the array. This method since
+    it iss going through a recursive call will take up more space and will typically run
+    slower since it's condition is based on 1. whether the search value is even
+    held in the array and 2. how far that value is from the center of the array. 
+*/
 
 int main() {
     Course C[10];
@@ -54,7 +74,11 @@ int main() {
     cout << "Enter 0-9 to Search a Course Id: ";
     cin >> target;
 
-    
+    int result = BinSearch(array, first, last, target);
+
+    if (result <= 9 || result == 0) {
+        
+    }
 
     /*temp test loop
     for (int i = 0; i < 10; i++) {
