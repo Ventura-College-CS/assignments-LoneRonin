0f6a1ca43c46a56f0aeb57039ab2e1e1fe6c06f6
LoneRonin <43432734+LoneRonin@users.noreply.github.com>
2021-12-12 17:57:04 -0800

    Update midterm.cpp
    That got rid of the errors

--- Midterm/midterm.cpp
+++ Midterm/midterm.cpp
@@ -3,6 +3,44 @@
 
 using namespace std;
 
+class Student {
+    private:
+        int SID;
+        string Sname;
+        vector<double> scores;
+    public:
+        Student() : SID(0), Sname() {}
+        Student(int i, string name, double *sc) {
+            SID = i;
+            Sname = name;
+            for(int j = 0; j<5; j++) { // possible idea... prehave all 5 objects made in line 16 and then have it just run each object idv.
+                scores.push_back(j); //come back to this to figure out how to have all 5
+                                     //Sstudent Objectyss be pushed
+            }
+        }
+        int getID() {
+            return SID;
+        }
+        string getName() {
+            return Sname;
+        }
+        vector<double> &getScores() {
+            return scores;
+        };
+        void setID(int ID) {
+            SID = ID;
+        }
+        void setName(string name) {
+            Sname = name;
+        }
+        void setScores(double *sc) {
+            //hmmmm maybe sstart on int main now
+            scores.clear();
+            for (int i = 0; i < 5; i++) {
+                scores.push_back(sc[i]);
+            }
+};
+
 int main () {
     Student s[5]; //Array of Class Students being initialized
     string name[] = {"Takeo", "Thy", "Yuumi", "Haru", "Irelia"}; //Array filled with nammes
@@ -12,7 +50,7 @@ int main () {
 
     Student tmp;
     vector<double> C;
-    Stack<Students, 10> CS;
+    Stack<Student, 10> CS;
 
    //need to take informtion from the Class and instead put it into a loop
    for (int i = 0; i < 5; i++) {
