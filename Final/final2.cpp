6dab6aa5480e51bcd7f2496762c7eea684783e7a
LoneRonin <43432734+LoneRonin@users.noreply.github.com>
2021-12-13 01:20:22 -0800

    Update final2.cpp
    Starting Question 2

--- Final/final2.cpp
+++ Final/final2.cpp
@@ -1,2 +1,31 @@
 #include <iostream>
 using namespace std;
+
+class Course {
+    private:
+        int cId;
+        string cName;
+        int cCred;
+    public:
+        Course() : cId(0), cName(), cCred() {
+
+        }
+        int getCId() const {
+            return cId;
+        }
+        string getCName() const {
+            return cName;
+        }
+        int getCCred() const {
+            return cCred;
+        }
+        void setCId(int id) {
+            cId = id;
+        }
+        void setCName(string name) {
+            cName = name;
+        }
+        void setCCred(int cred) {
+            cCred = cred;
+        }
+};
